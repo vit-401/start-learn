85f40f4452cbaacf7d681f7b0b853dcbe4a11e94
vitalii.prysiazhniuk <vitalii.prysiazhniuk@attractgroup.com>
2023-03-21 22:00:26 +0200

    first commit

--- src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import {UserAccountDBType} from '../repositories/types'
-
-declare global {
-    declare namespace Express {
-        export interface Request {
-            user: UserAccountDBType | null
-        }
-    }
-}
