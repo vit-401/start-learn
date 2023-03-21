4007be54a98fc339c10f268fb3abf0e66cc9e904
vit-401 <vitalii.prysiazhniuk@attractgroup.com>
2023-03-21 22:32:01 +0200

    Project init

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
-import 'dotenv/config'
 import express from 'express'
-import bodyParser from 'body-parser'
-import {runDb} from './repositories/db'
-import {authRouter} from './routes/auth/auth-router'
-import {usersRouter} from './routes/users/users-router'
 
 const app = express()
 
-const jsonBodyMiddleware = bodyParser.json()
-app.use(jsonBodyMiddleware)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
-app.use('/users', usersRouter)
-app.use('/auth', authRouter)
 
 const startApp = async () => {
-    await runDb()
-    app.listen(port, () => {
-        console.log(`App started: ${port}`)
-    })
+
+  app.listen(port, () => {
+    console.log(`App started: ${port}`)
+  })
 }
 
 startApp()
