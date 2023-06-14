3ce427bcbeef97364e45e5822d1e0e926af941c5
vitalii.prysiazhniuk <vitalii.prysiazhniuk@attractgroup.com>
2023-06-14 20:19:32 +0300

    Added tests

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import express from 'express'
-import productsRoute from "./routes/protuctRouter";
 import runDB from "./db";
-import errorHandler from "./middleware/errorHandler";
-import {ProductService} from "./service/productService";
-import {authRouter} from "./routes/authRouter";
-import UserRepository from "./repository/userRepository";
-import ProductRepository from "./repository/productRepository";
-import UserService from "./service/userService";
-import {authMiddleware} from "./middleware/authMiddleware";
 import {ObjectId} from "mongodb";
-import {emailRouter} from "./routes/emailRouter";
-import useragent from "express-useragent";
-import cors from "cors";
-import {rateLimiterMiddleware} from "./middleware/rateLimiterMiddleware";
-import {ProductModel} from "./schemas/product-model";
+import {app} from "./setting";
 
 
-const bodyParser = require('body-parser')
-const app = express()
-const userRepository = new UserRepository()
-const productRepository = new ProductRepository(ProductModel)
 
 declare global {
   namespace Express {
@@ -29,23 +12,6 @@ declare global {
   }
 }
 
-const productService = new ProductService(productRepository)
-const userService = new UserService(userRepository)
-
-
-app.use(cors({origin: '*'}))
-app.use(errorHandler);
-app.use(useragent.express());
-
-app.use(bodyParser.json());
-app.use(rateLimiterMiddleware)
-app.use('/auth', authRouter(userService))
-app.use('/products',authMiddleware,  productsRoute(productService))
-app.use('/email', authMiddleware, emailRouter)
-
-
-// Error handler middleware
-app.use(errorHandler);
 
 const port = process.env.PORT || 5001
 
---
+++ src/setting.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import UserRepository from "./repository/userRepository";
+import ProductRepository from "./repository/productRepository";
+import {ProductModel} from "./schemas/product-model";
+import {ProductService} from "./service/productService";
+import UserService from "./service/userService";
+import cors from "cors";
+import errorHandler from "./middleware/errorHandler";
+import useragent from "express-useragent";
+import {rateLimiterMiddleware} from "./middleware/rateLimiterMiddleware";
+import {authRouter} from "./routes/authRouter";
+import {authMiddleware} from "./middleware/authMiddleware";
+import productsRoute from "./routes/protuctRouter";
+import {emailRouter} from "./routes/emailRouter";
+
+const bodyParser = require('body-parser')
+
+export const app = express()
+const userRepository = new UserRepository()
+const productRepository = new ProductRepository(ProductModel)
+const productService = new ProductService(productRepository)
+const userService = new UserService(userRepository)
+
+
+app.use(cors({origin: '*'}))
+app.use(errorHandler);
+app.use(useragent.express());
+
+app.use(bodyParser.json());
+app.use(rateLimiterMiddleware)
+app.use('/auth', authRouter(userService))
+app.use('/products',authMiddleware,  productsRoute(productService))
+app.use('/email', authMiddleware, emailRouter)
+
+
+// Error handler middleware
+app.use(errorHandler);
